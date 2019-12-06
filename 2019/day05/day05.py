904fa1efaa804be04af8c4517a033342b58eb642
Eric Garnick <eric.b.garnick@gmail.com>
2019-12-05 20:30:34 -0800

    2019-day05 slight refactor

--- 2019/day05/day05.py
+++ 2019/day05/day05.py
@@ -17,7 +17,19 @@ class IntcodeComputerV2(IntcodeComputerV1):
         instruction_code = program[idx_val]
         op_code, mode1, mode2, mode3 = IntcodeComputerV2._interpret_instruction(instruction_code)
 
-        if op_code not in IntcodeComputerV2.PARAMETERIZED_PROCESSES:
+        if op_code in IntcodeComputerV2.PARAMETERIZED_PROCESSES:
+            if op_code == 3:
+                tgt_idx = program[idx_val + 1]
+                param = int(input("Please enter your input value: "))
+                program[tgt_idx] = param
+            else:
+                # op_code == 4
+                tgt_idx = IntcodeComputerV2._input_for_mode(mode1, 1, idx_val, program)
+                print(tgt_idx)
+
+            next_code_idx[0] += 2
+
+        else:
             input1 = IntcodeComputerV2._input_for_mode(mode1, 1, idx_val, program)
             input2 = IntcodeComputerV2._input_for_mode(mode2, 2, idx_val, program)
             output = program[idx_val + 3]
@@ -50,19 +62,6 @@ class IntcodeComputerV2(IntcodeComputerV1):
                 else:
                     program[output] = 0
                 next_code_idx[0] += 4
-
-        else:
-            if op_code == 3:
-                tgt_idx = program[idx_val + 1]
-                param = int(input("Please enter your input value: "))
-                program[tgt_idx] = param
-            elif op_code == 4:
-                tgt_idx = IntcodeComputerV2._input_for_mode(mode1, 1, idx_val, program)
-                print(tgt_idx)
-            else:
-                raise ValueError(f"Unknown op code {op_code}")
-
-            next_code_idx[0] += 2
 
     @staticmethod
     def _input_for_mode(mode: int, mode_pos: int, program_idx: int,
