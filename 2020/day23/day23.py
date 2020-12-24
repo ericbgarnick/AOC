ea0a411c4113348623e753135b4dcf7b6b7bc83a
Eric Garnick <eric@energysage.com>
2020-12-24 16:13:49 -0500

    Day 23 part 2

--- 2020/day23/day23.py
+++ 2020/day23/day23.py
@@ -1,54 +1,65 @@
-from typing import List
+from typing import Dict
 
+# Real start values
 CUP_LABELS = [3, 1, 8, 9, 4, 6, 5, 7, 2]
+# Sample start values
+# CUP_LABELS = [3, 8, 9, 1, 2, 5, 4, 6, 7]
 
 
-def play(num_moves, max_cup: int) -> str:
-    history = {}
-    cups = [cup for cup in CUP_LABELS + list(range(10, max_cup + 1))]
-    cur_idx = 0
+def play(num_moves, max_cup: int = 9, part_num: int = 1) -> int:
+    """Linked list of dicts for cups, extra dict for constant-time access to cups"""
+    prev_cup = {"val": CUP_LABELS[0], "next": {}}
+    cup_map = {CUP_LABELS[0]: prev_cup}
+    for cup_label in CUP_LABELS[1:]:
+        cur_cup = {"val": cup_label, "next": {}}
+        cup_map[cup_label] = cur_cup
+        prev_cup["next"] = cur_cup
+        prev_cup = cur_cup
+
+    for addl_cup_labal in range(10, max_cup + 1):
+        cur_cup = {"val": addl_cup_labal, "next": {}}
+        cup_map[addl_cup_labal] = cur_cup
+        prev_cup["next"] = cur_cup
+        prev_cup = cur_cup
+
+    prev_cup["next"] = cup_map[CUP_LABELS[0]]
+
+    cur_cup = cup_map[CUP_LABELS[0]]
     for i in range(num_moves):
-        cur_cup = cups[cur_idx]
-        cups = move_cups(cups, cur_idx)
-        cur_idx = (cups.index(cur_cup) + 1) % len(cups)
+        move_cups(cur_cup, cup_map, max_cup)
+        cur_cup = cur_cup["next"]
 
-        # one_idx = cups.index(1)
-        # as_str = "-".join([str(cup) for cup in cups[one_idx + 1:] + cups[:one_idx]][:2])
-        # if as_str in history:
-        #     if i > -1:
-        #         print(f"repeat at {i} from {history[as_str]}: {as_str}")
-        #     history[as_str].add(i)
-        # else:
-        #     history[as_str] = {i}
-
-    return after_1(cups)
+    return after_1(cup_map, part_num)
 
 
-def move_cups(cups: List[int], cur_idx: int) -> List[int]:
-    cur_cup = cups[cur_idx]
+def move_cups(cur_cup: Dict, cup_map: Dict, max_cup: int):
+    first_pick = cur_cup["next"]
+    last_pick = first_pick["next"]["next"]
 
-    pick1 = cups[(cur_idx + 1) % len(cups)]
-    pick2 = cups[(cur_idx + 2) % len(cups)]
-    pick3 = cups[(cur_idx + 3) % len(cups)]
+    destination = cur_cup["val"] - 1 or max_cup
+    while destination in [first_pick["val"], first_pick["next"]["val"], last_pick["val"]]:
+        destination = destination - 1 or max_cup
 
-    destination = cur_cup - 1 or 9
-    while destination in [pick1, pick2, pick3]:
-        destination = destination - 1 or 9
-
-    reduced_cups = [cup for cup in cups if cup not in [pick1, pick2, pick3]]
-    dest_idx = reduced_cups.index(destination)
-    cups = reduced_cups[:dest_idx + 1] + [pick1, pick2, pick3] + reduced_cups[dest_idx + 1:]
-
-    return cups
+    dest_cup = cup_map[destination]
+    cur_cup["next"] = last_pick["next"]
+    last_pick["next"] = dest_cup["next"]
+    dest_cup["next"] = first_pick
 
 
-def after_1(cups: List[int], cutoff: int = 2) -> str:
-    one_idx = cups.index(1)
-    return "-".join([str(cup) for cup in cups[one_idx + 1:] + cups[:one_idx]][:cutoff])
+def after_1(cup_map: Dict, part_num: int) -> int:
+    one_cup = cup_map[1]
+    if part_num == 1:
+        result = []
+        next_cup = one_cup["next"]
+        while next_cup["val"] != 1:
+            result.append(str(next_cup["val"]))
+            next_cup = next_cup["next"]
+        return int("".join(result))
+    else:
+        return one_cup['next']['val'] * one_cup['next']['next']['val']
 
 
 if __name__ == "__main__":
-    from time import time
-    start = time()
-    print(play(200, 1_000_000))
-    print("RUNTIME:", time() - start)
+    print("PART 1:", play(100))
+    # Part 2 takes about 15s
+    print("PART 2:", play(10_000_000, 1_000_000, 2))
