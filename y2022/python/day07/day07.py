ef1a4a0130eac87af01bf071794e58b28ee99c54
Eric Garnick <eric.b.garnick@gmail.com>
2022-12-07 16:42:01 -0500

    Day 7 python

---
+++ y2022/python/day07/day07.py
@@ -0,0 +1,124 @@
+from enum import Enum
+from typing import Set, TextIO, Optional, Dict
+
+from y2022.python.shared import get_data_file_path
+
+FILESYSTEM_SIZE = 70000000
+MAX_DIR_SIZE = 100000
+COMMAND_PROMPT = "$"
+ROOT_DIR_PATH = "/"
+ROOT_DIR_NAME = ""
+PARENT_DIR = ".."
+
+
+class NodeType(Enum):
+    file = "file"
+    directory = "directory"
+
+class Node:
+    def __init__(
+        self, name: str,
+        node_type: NodeType,
+        parent: Optional['Node'] = None,
+        size: int = 0,
+    ):
+        self.name = name
+        self.parent = parent
+        self._node_type = node_type
+        self._contents: Set['Node'] = set()
+        self._size = size
+
+    def __str__(self) -> str:
+        suffix = "/" if self._node_type == NodeType.directory else f" (self._size)"
+        return self.name + suffix
+
+    @property
+    def size(self) -> int:
+        if self._node_type == NodeType.file:
+            return self._size
+        return sum(c.size for c in self._contents)
+
+    @property
+    def contents(self) -> Optional[Set[str]]:
+        if self._node_type == NodeType.file:
+            return None
+        return {c.name for c in self._contents}
+
+    @property
+    def path(self) -> Optional[str]:
+        if self._node_type == NodeType.file:
+            return None
+        if self.parent:
+            path = self.parent.path
+        else:
+            path = ""
+        return path + f"{self.name}/"
+
+    def add_contents(self, file_or_dir: 'Node'):
+        self._contents.add(file_or_dir)
+
+
+def parse_terminal_output(buffer: TextIO) -> Dict[str, Node]:
+    """
+    Return a mapping of path/to/directory/ mapped to the Node for that directory.
+    """
+    cur_dir = Node(ROOT_DIR_NAME, NodeType.directory)
+    dir_map = {cur_dir.path: cur_dir}
+    for line in buffer:
+        if line[0] == COMMAND_PROMPT:
+            command = line.strip().split()[1:]
+            if command[0] == "cd":
+                cur_dir = change_directory(command[1], cur_dir, dir_map)
+            else:
+                # Move to the next line after 'ls'
+                continue
+        else:
+            record_object(line.strip(), cur_dir, dir_map)
+    return dir_map
+
+
+def change_directory(dir_name: str, cur_dir: Node, dir_map: Dict[str, Node]) -> Node:
+    if dir_name == PARENT_DIR:
+        cur_dir = cur_dir.parent
+    elif dir_name == ROOT_DIR_PATH:
+        cur_dir = dir_map[ROOT_DIR_PATH]
+    else:
+        cur_path = cur_dir.path
+        cur_dir = dir_map[cur_path + f"{dir_name}/"]
+    return cur_dir
+
+
+def record_object(output_line: str, cur_dir: Node, dir_map: Dict[str, Node]):
+    """
+    Create a new Node for output_line.
+
+    For files, add the new Node to cur_dir contents.
+    For directories, add the new Node to dir_map keyed by its absolute path.
+    """
+    dir_or_size, node_name = output_line.split()
+    if dir_or_size == "dir":
+        new_node = Node(node_name, NodeType.directory, parent=cur_dir)
+        dir_map[new_node.path] = new_node
+    else:
+        new_node = Node(node_name, NodeType.file, size=int(dir_or_size))
+    if node_name not in cur_dir.contents:
+        cur_dir.add_contents(new_node)
+
+def main():
+    with open(get_data_file_path(__file__), "r") as f_in:
+        dir_map = parse_terminal_output(f_in)
+
+    # PART 1
+    total_size = sum(d.size for d in dir_map.values() if d.size <= MAX_DIR_SIZE)
+    print("PART 1:", total_size)
+
+    # PART 2
+    required_space = 30000000
+    unused_space = FILESYSTEM_SIZE - dir_map[ROOT_DIR_PATH].size
+    min_del_size = required_space - unused_space
+    target_dir_size = min(d.size for d in dir_map.values() if d.size >= min_del_size)
+    print("PART 2:", target_dir_size)
+
+
+if __name__ == "__main__":
+    main()
---
+++ y2022/python/tests/test_day07.py
@@ -0,0 +1,70 @@
+import pytest
+
+from y2022.python.day07.day07 import Node, NodeType, change_directory
+
+
+def test_node_size():
+    # GIVEN
+    root_dir = Node("", NodeType.directory)
+    file_1 = Node("file_1", NodeType.file, size=11)
+    subdir = Node("subdir", NodeType.directory, parent=root_dir)
+    file_2 = Node("file_2", NodeType.file, size=22)
+    file_3 = Node("file_3", NodeType.file, size=33)
+
+    root_dir.add_contents(file_1)
+    root_dir.add_contents(subdir)
+    subdir.add_contents(file_2)
+    subdir.add_contents(file_3)
+
+    # THEN
+    assert file_1.size == 11
+    assert file_2.size == 22
+    assert file_3.size == 33
+    assert subdir.size == 55
+    assert root_dir.size == 66
+
+
+def test_node_path():
+    # GIVEN
+    root_dir = Node("", NodeType.directory)
+    file_1 = Node("file_1", NodeType.file, size=11)
+    subdir = Node("subdir", NodeType.directory, parent=root_dir)
+    file_2 = Node("file_2", NodeType.file, size=22)
+    file_3 = Node("file_3", NodeType.file, size=33)
+
+    root_dir.add_contents(file_1)
+    root_dir.add_contents(subdir)
+    subdir.add_contents(file_2)
+    subdir.add_contents(file_3)
+
+    # THEN
+    assert file_1.path is None
+    assert file_2.path is None
+    assert file_3.path is None
+    assert subdir.path == "/subdir/"
+    assert root_dir.path == "/"
+
+
+@pytest.mark.parametrize(
+    "dest_dir_name,cur_dir_path,expected_dir_path",
+    [
+        pytest.param("/", "/subdir/subsubdir/", "/", id="jump to root"),
+        pytest.param("subsubdir", "/subdir/", "/subdir/subsubdir/", id="descend into subdirectory"),
+        pytest.param("..", "/subdir/subsubdir/", "/subdir/", id="ascend to parent directory"),
+    ]
+)
+def test_change_directory(dest_dir_name, cur_dir_path, expected_dir_path):
+    # GIVEN
+    root_dir = Node("", NodeType.directory)
+    subdir = Node("subdir", NodeType.directory, parent=root_dir)
+    subsubdir = Node("subsubdir", NodeType.directory, parent=subdir)
+
+    dir_map = {
+        root_dir.path: root_dir, subdir.path: subdir, subsubdir.path: subsubdir
+    }
+
+    # WHEN
+    new_dir = change_directory(dest_dir_name, dir_map[cur_dir_path], dir_map)
+
+    # THEN
+    assert new_dir == dir_map[expected_dir_path]
